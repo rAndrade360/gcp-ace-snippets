a2b54c6dfb5f3400c1ccf20f6150852683cd025e
Renan de Andrade <renan.correa@engdb.com.br>
2023-01-19 21:11:47 -0300

    use webhook

--- use-cases/img2txt/bot/main.go
+++ use-cases/img2txt/bot/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+		log.Printf("Defaulting to PORT %s", PORT)
+	}
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Panic(err)
@@ -15,12 +22,15 @@ func main() {
 
 	bot.Debug = true
 
+	updates := bot.ListenForWebhook("/bot")
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
+
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
+	go http.ListenAndServe(":"+PORT, nil)
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
