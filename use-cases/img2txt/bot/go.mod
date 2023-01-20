0c778e41292cab97e19ea8a6b9868b277a5dae89
Renan de Andrade <renan.correa@engdb.com.br>
2023-01-19 21:51:14 -0300

    img 2 txt

--- use-cases/img2txt/bot/go.mod
+++ use-cases/img2txt/bot/go.mod
@@ -2,4 +2,7 @@ module github.com/rAndrade360/gcp-ace-snippets/use-cases/img2txt/bot
 
 go 1.19
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
--- use-cases/img2txt/bot/main.go
+++ use-cases/img2txt/bot/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"image/jpeg"
 	"log"
+	"math"
 	"net/http"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/nfnt/resize"
 )
+
+var scale = []string{" ", ".", ",", "-", "~", "+", "=", "7", "8", "9", "$", "W", "#", "@", "Ñ"}
 
 func main() {
 	PORT := os.Getenv("PORT")
@@ -38,16 +45,68 @@ func main() {
 
 			if len(update.Message.Photo) > 0 {
 				msg := tgbotapi.NewPhoto(update.Message.Chat.ID, tgbotapi.FileID(update.Message.Photo[0].FileID))
+
 				msg.ReplyToMessageID = update.Message.MessageID
 
-				bot.Send(msg)
+				f, err := bot.GetFile(tgbotapi.FileConfig{
+					FileID: update.Message.Photo[0].FileID,
+				})
+				if err != nil {
+					log.Println("Err to get file: ", err.Error())
+					bot.Send(msg)
+					continue
+				}
+
+				res, err := http.Get(f.Link(os.Getenv("BOT_TOKEN")))
+				if err != nil {
+					log.Println("Err to get file: ", err.Error())
+					bot.Send(msg)
+					continue
+				}
+
+				imge, err := jpeg.Decode(res.Body)
+				if err != nil {
+					log.Fatal("Err to decode img: ", err.Error())
+					bot.Send(msg)
+					continue
+				}
+
+				txt := GenerateASCII(imge)
+
+				msgTxt := tgbotapi.NewMessage(update.Message.Chat.ID, txt)
+				msgTxt.ReplyToMessageID = update.Message.MessageID
+
+				bot.Send(msgTxt)
 				continue
 			}
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			txt := fmt.Sprintf("Olá %s, que tal enviar um imagem para ver um truque de mágica?", update.Message.From.FirstName)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, txt)
 			msg.ReplyToMessageID = update.Message.MessageID
 
 			bot.Send(msg)
 		}
 	}
+}
+
+func GenerateASCII(imge image.Image) string {
+	imge = resize.Resize(85, 40, imge, resize.Lanczos2)
+
+	txt := ""
+
+	for y := imge.Bounds().Min.Y; y <= imge.Bounds().Max.Y; y++ {
+		for x := imge.Bounds().Min.X; x <= imge.Bounds().Max.X; x++ {
+			c := imge.At(x, y)
+			r, g, b, _ := c.RGBA()
+
+			gray := (r + g + b) / 3
+
+			char := int(math.Round((float64(gray) / 65536) * float64(len(scale)-1)))
+			txt += string(scale[char])
+		}
+		txt += "\n"
+	}
+
+	return txt
 }
